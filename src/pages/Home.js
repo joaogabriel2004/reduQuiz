99bc6c6842d452b492dbbed87ee35115e20bc286
João Gabriel <joaogabrielsj2004@gmail.com>
2024-04-29 13:42:31 -0300

    create: Código base - tela inicial do quiz

---
+++ src/pages/Home.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+import iconeCorra from '../assets/ícone_branco.png'
+
+import '../style/global.css';
+import '../style/homepage.css';
+
+function HomePage() {
+    return (
+        <div className='container'>
+            <div className='div-left'>
+                <img id="iconeCorra" src={iconeCorra} width='130px'></img>
+                <h3 className='h3_Titulo'>ReduQuiz de Danos</h3>
+
+                <p className='p_descricao'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque a tristique leo, vel malesuada felis. Praesent posuere sagittis nunc, et efficitur nibh. Sed a libero in orci condimentum congue a elementum nibh. Integer id mauris ut justo rhoncus scelerisque. Aliquam eu lorem cursus, bibendum erat feugiat, tincidunt eros. In sit amet tortor porttitor, dictum tortor vitae, maximus lectus. Suspendisse suscipit semper purus, sit amet convallis nulla faucibus eu. Aenean viverra posuere tincidunt.</p>                
+                <h4 className='h4_ComoJogar'>COMO JOGAR</h4>
+                <p><strong>1.</strong></p>
+                <p><strong>2.</strong></p>
+                <p><strong>3.</strong></p>
+                <p><strong>4.</strong></p>
+                <p><strong>5.</strong></p>
+
+                <h3 className='h3_SeDivirta'>SE DIVIRTA!</h3>
+
+            </div>
+            <div className='div-right'>
+                <input className='input_Nome' type='text' placeholder='Coloque aqui o seu nome'></input><br/>
+                <Link to="/Quiz">
+                    <button>Começar a jogar</button>
+                </Link>
+            </div>
+        </div>
+    );
+}
+
+export default HomePage;
